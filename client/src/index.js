07892b5886ba94d7dbb5e976340b15add5d6ba88
Scott Heng <scottheng3@gmail.com>
2017-05-02 01:20:44 -0700

    create client-side routes

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import '../dist/css/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+
+filepicker.setKey("AjV85FvfTBCAO5mHJYumQz");
+
+ReactDOM.render(
+	Routes,
+	document.getElementById('content')
+);
